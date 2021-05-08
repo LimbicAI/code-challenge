967d445e6d485e97c3dbd51a229901ffd33149c8
akarpas <andreasmkar@gmail.com>
2021-05-08 13:03:37 +0300

    feat: setup basic App with routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Clients from './screens/Clients';
+import Questions from './screens/Questions';
+import styles from './App.module.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className={styles.app}>
+			<header className={styles.appHeader}>
+				<h2>Therapy Portal</h2>
+			</header>
+			<Router>
+				<Switch>
+					<Route exact path="/questions" component={Questions} />
+					<Route path="/clients" component={Clients} />
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
