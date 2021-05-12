e1e341d71ce761dbb81f364cc485702726cea4ad
akarpas <andreasmkar@gmail.com>
2021-05-13 00:13:59 +0300

    chore: formatting

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -6,34 +6,34 @@ import styles from './styles.module.scss';
 const noop = () => {};
 
 const Button = ({
-	children,
-	disabled,
-	onClick = noop,
-	type = 'button',
-	variation = 'default'
+  children,
+  disabled,
+  onClick = noop,
+  type = 'button',
+  variation = 'default'
 }) => {
-	return (
-		<button
-			className={clsx(styles.button, {
-				[styles.default]: variation === 'default',
-				[styles.small]: variation === 'small',
-				[styles.alert]: variation === 'alert',
-				[styles.disabled]: disabled
-			})}
-			disabled={disabled}
-			onClick={onClick}
-			type={type}
-		>
-			{children}
-		</button>
-	);
+  return (
+    <button
+      className={clsx(styles.button, {
+        [styles.default]: variation === 'default',
+        [styles.small]: variation === 'small',
+        [styles.alert]: variation === 'alert',
+        [styles.disabled]: disabled
+      })}
+      disabled={disabled}
+      onClick={onClick}
+      type={type}
+    >
+      {children}
+    </button>
+  );
 };
 
 Button.propTypes = {
-	children: PropTypes.node,
-	disabled: PropTypes.bool,
-	onClick: PropTypes.func,
-	variation: PropTypes.oneOf(['default', 'small', 'alert'])
+  children: PropTypes.node,
+  disabled: PropTypes.bool,
+  onClick: PropTypes.func,
+  variation: PropTypes.oneOf(['default', 'small', 'alert'])
 };
 
 export default Button;
