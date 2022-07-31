c9329ba85471e7ae8e67939c5df104bdffc10cec
Macanthony Ahaotu <33723276+macvish@users.noreply.github.com>
2022-07-31 13:59:49 +0100

    Add login page/landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Box, Button, Divider, Flex, Heading, Stack, Text } from '@chakra-ui/react'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Flex>
+      <Box w="55%" h="100vh" backgroundColor="#F98BA4"></Box>
+      <Flex w="45%" h="100vh" alignItems="center" justifyContent="center">
+        <Flex
+          shadow="2xl"
+          p="12"
+          width='65%'
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          rounded={5}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Heading mb={5}>Log in</Heading>
+          <Text mb={3} textAlign="center">Please Select An Account.</Text>
+            <Button colorScheme="gray" w="100%">Client</Button>
+          <Stack direction='row' p={3}>
+            <Divider />
+            <Text>Or</Text>
+            <Divider />
+          </Stack>
+            <Button colorScheme="gray" w="100%">Admin</Button>
+        </Flex>
+      </Flex>
+      </Flex>
+  )
 }
 
-export default App;
+export default App
