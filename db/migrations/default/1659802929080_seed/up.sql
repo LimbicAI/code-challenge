2bd8763d0861cd12fb96ef5f5328724a89d71e5a
Ali Sharafi <ali.sharafi.github@gmail.com>
2022-08-06 20:52:46 +0430

    feat: seed data

---
+++ db/migrations/default/1659802929080_seed/up.sql
@@ -0,0 +1,5 @@
+Insert Into questions(type,text) values 
+('text', 'What is you name?'),
+('number', 'How old are you?'),
+('multiLine', 'How do you feel when you wake up in the mornings?'),
+('multiLine', 'Are you happy with you job?');
